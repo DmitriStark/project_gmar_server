fc6577d240541c93514a78b834147bd7d7667654
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-01-30 21:10:43 +0200

    other way to connect

--- db.js
+++ db.js
@@ -4,12 +4,19 @@ const mongoose = require('mongoose');
 const protocol = 'mongodb+srv';
 
 function connect() {
-	mongoose.connect(`${protocol}://${process.env.ATLAS_HOST}`, {
-		user: process.env.ATLAS_USER,
-		pass: process.env.ATLAS_PASS,
-		dbName: 'test'
-	});
+    mongoose.connect(`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@${process.env.ATLAS_HOST}/test`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    db.once('open', () => {
+        console.log('Connected to MongoDB Atlas!');
+    });
 }
+
 
 function disconnect() {
 	mongoose.connection.close();
