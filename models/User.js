4b1af5ed7c10f67217c0b26fc697244dd1a92d36
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-02-01 19:35:42 +0200

    removed ver and id from users

--- models/User.js
+++ models/User.js
@@ -3,6 +3,16 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
 	name: String,
 	age: Number,
-});
+},{
+	toJSON: {
+		virtuals: true,
+		versionKey: false,
+		transform: function (doc, ret) {
+			ret.id = ret._id; // Rename '_id' to 'id'
+			delete ret._id; // Remove the '_id' property
+		}
+	}
+	});
+
 
 module.exports = mongoose.model('users', userSchema);
